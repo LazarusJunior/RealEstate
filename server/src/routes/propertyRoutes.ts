83a3bbce007665df0963fcd04b136615c288223e
LazarusJunior <keyajunior9@gmail.com>
2024-11-07 10:06:19 +0300

    updated routes

--- server/src/routes/propertyRoutes.ts
+++ server/src/routes/propertyRoutes.ts
@@ -2,16 +2,14 @@ import { Router } from 'express';
 import { createProperty, deleteProperty, getProperties, getPropertyById, updateProperty } from '../controllers/propertyController';
 import { authenticateJWT } from '../middleware/authMiddleware';
 import { adminCheck } from '../middleware/adminCheck';
-import { auditLogMiddleware } from '../middleware/auditLogMiddleware';
 import { getPropertyPerformance } from '../controllers/reportController';
 
 
 const router = Router();
 
-// This route requires authentication
-router.post('/createProperty', authenticateJWT, adminCheck, auditLogMiddleware, createProperty); // Create property
-router.patch('/updateProperty/:id', authenticateJWT, adminCheck, auditLogMiddleware, updateProperty); // Update property
-router.delete('/deleteProperty/:id', authenticateJWT, adminCheck, auditLogMiddleware, deleteProperty); // Delete property
+router.post('/createProperty', authenticateJWT, adminCheck,  createProperty); // Create property
+router.patch('/updateProperty/:id', authenticateJWT, adminCheck, updateProperty); // Update property
+router.delete('/deleteProperty/:id', authenticateJWT, adminCheck,  deleteProperty); // Delete property
 
 
 router.get('/getProperties', getProperties);// View all properties
